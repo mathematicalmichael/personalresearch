d8f7ae28cdd9df0076b1ce1b8027e4e667168c2a
Michael Pilosov <consistentbayes@gmail.com>
2019-04-10 23:42:31 +0000

    linting.

--- Notebooks/ECNoise.py
+++ Notebooks/ECNoise.py
@@ -1,70 +1,72 @@
-## ECNoise: (f,x_b,M,mult) -> (sigma_hat, fvals)
-## Inputs: noisy black-box function f; base point x_b, Px1 col vec;
-##         M number of samples to use, M must be >2; a handle for whether we have add/mult noise.
-##	   We default to additive noise, so we set mult=false. user can set mult=true if needed.
-## Outputs: an estimator to the variance in noise of f sigma_hat;
-##          and the M function values stored in fvals
+# ECNoise: (f,x_b,M,mult) -> (sigma_hat, fvals)
+# Inputs: noisy black-box function f; base point x_b, Px1 col vec;
+# M number of samples to use, M must be >2; a handle for whether we have add/mult noise.
+# We default to additive noise, so we set mult=false. user can set mult=true if needed.
+# Outputs: an estimator to the variance in noise of f sigma_hat;
+# and the M function values stored in fvals
 
-## Cost: M evals of f
+# Cost: M evals of f
 
 import numpy as np
 
+
 def ECNoise(f, x_b, M, mult=False):
-	#determine length scale from max norm of x_b
-	#h=1E-2*np.linalg.norm(x_b,ord=np.inf)
-	h=0.01
-	# Throw error for M too small
-	if M<=2:
-		return print('Please choose M>2.')
+    # determine length scale from max norm of x_b
+    # h=1E-2*np.linalg.norm(x_b,ord=np.inf)
+    h = 0.01
+    # Throw error for M too small
+    if M <= 2:
+        return print('Please choose M>2.')
 
-	# Grab dim(domain(f))=:P
-	P=np.shape(x_b)[0]	
+    # Grab dim(domain(f))=:P
+    P = np.shape(x_b)[0]
 
-	# Hard-coded normalized direction to sample in
-	if x_b.ndim > 1:
-		#p_u=np.ones((P,1))
-		p_u=np.random.rand(P,1)
-	else:
-		#p_u=np.ones(P)
-		p_u=np.random.rand(P)
-	
-	p=p_u/np.linalg.norm(p_u)
+    # Hard-coded normalized direction to sample in
+    if x_b.ndim > 1:
+        # p_u=np.ones((P,1))
+        p_u = np.random.rand(P, 1)
+    else:
+        # p_u=np.ones(P)
+        p_u = np.random.rand(P)
 
-	print(p.shape,x_b.shape)
+    p = p_u/np.linalg.norm(p_u)
 
-	# Form difference table T
-	xvals=np.copy(x_b)	
-	T=np.zeros((M,M))
-	for i in range(0,M):
-		intx = x_b + (i*h)*p
-		if i>=1:
-			xvals=np.hstack((xvals,intx))
-		else:
-			xvals=xvals
-        	
-		T[i,0] = f(intx)
-	for j in range(0,M-1):
-    		for i in range(0,M-j-1):
-        		T[i,j+1] = T[i+1,j] - T[i,j]
-	
-	fvals=T[:,0] # store the f evals
+    print(p.shape, x_b.shape)
 
-	# Make a row vector to store the k-level estimators (sigma_k^2) 
-	# ie: Initialize empty vector for storage, row vector for readability
-	S = np.zeros((1,M))
+    # Form difference table T
+    xvals = np.copy(x_b)
+    T = np.zeros((M, M))
+    for i in range(0, M):
+        intx = x_b + (i*h)*p
+        if i >= 1:
+            xvals = np.hstack((xvals, intx))
+        else:
+            xvals = xvals
 
-	# Build S according to paper; each k-th component of S is the k-th level estimator for the variance in our noise
-	# which is computed using a scaled average of the k-th level difference values, from the difference table T
-	for i in range(1,M):
-    		S[0,i] = ((np.math.factorial(i)**2.)/np.math.factorial(2*i))*(1./(M-i))*np.sum(T[:,i]**2,axis=0)
-	
-	S=S[:,1:] # Don't need the first column (because first col. of T just holds function values!)
+        T[i, 0] = f(intx)
+    for j in range(0, M-1):
+        for i in range(0, M-j-1):
+            T[i, j+1] = T[i+1, j] - T[i, j]
 
-	# Form our estimator which averages the values that are usually good estimates
-	sigma_hat=np.sum(S[:,1:M-1], axis=1)/(M-2)
+    fvals = T[:, 0]  # store the f evals
 
-	
-	if mult is False:
-		return [sigma_hat, xvals, fvals]
-	else:
-		return [sigma_hat/fvals[0]**2, xvals, fvals]
+    # Make a row vector to store the k-level estimators (sigma_k^2)
+    # ie: Initialize empty vector for storage, row vector for readability
+    S = np.zeros((1, M))
+
+    # Build S according to paper; each k-th component of S is the k-th level estimator for the variance in our noise
+    # which is computed using a scaled average of the k-th level difference values, from the difference table T
+    for i in range(1, M):
+        S[0, i] = ((np.math.factorial(i)**2.)/np.math.factorial(2*i)
+                   )*(1./(M-i))*np.sum(T[:, i]**2, axis=0)
+
+    # Don't need the first column (because first col. of T just holds function values!)
+    S = S[:, 1:]
+
+    # Form our estimator which averages the values that are usually good estimates
+    sigma_hat = np.sum(S[:, 1:M-1], axis=1)/(M-2)
+
+    if mult is False:
+        return [sigma_hat, xvals, fvals]
+    else:
+        return [sigma_hat/fvals[0]**2, xvals, fvals]
--- Notebooks/dfo_v1.py
+++ Notebooks/dfo_v1.py
@@ -1,130 +1,104 @@
 import numpy as np
 
 
-def get_mu_star(var,L_1,N):
-    
-	return ((8*var*N)/(L_1**2*(N+6)**3))**0.25
+def get_mu_star(var, L_1, N):
+
+    return ((8*var*N)/(L_1**2*(N+6)**3))**0.25
 
 
+def get_h(L1, N):
 
-def get_h(L1,N):
-
-	return 1/(4*L1*(N+4))
-
-
-def get_mult_mu_star(var,L_1,N):
-	return ((16*var*N)/((L_1**2)*(1+3*var)*(N+6)**3))**0.25
+    return 1/(4*L1*(N+4))
 
 
-def STARS(x_init,F,mu_star,h,active=None,mult=False,wts=None):
-    
-	'''     
-	x_init: initial x value, size Px1
-    
-	F: function we wish to minimize
+def get_mult_mu_star(var, L_1, N):
+    return ((16*var*N)/((L_1**2)*(1+3*var)*(N+6)**3))**0.25
+
+
+def STARS(x_init, F, mu_star, h, active=None, mult=False, wts=None):
+    '''     
+    x_init: initial x value, size Px1
+
+    F: function we wish to minimize
 ; note that lower f's are evaluations of F(*)
-    
-	mu_star: smoothing parameter
-    
-	h: step length 
-    
-	active: Should be size Pxk
+
+    mu_star: smoothing parameter
+
+    h: step length 
+
+    active: Should be size Pxk
 ; default is none
-	mult: Set to true if mult noise; default is add noise
-	wts: Should be size Px1, filled with the weights
-	     Default is None.
-	     If using active vars, user should provide wts=ss.eigenvals    
-    
-	'''
-    
-    
-	# Evaluate noisy F(x_init)
-    
-	f0 = F(x_init)
+    mult: Set to true if mult noise; default is add noise
+    wts: Should be size Px1, filled with the weights
+         Default is None.
+         If using active vars, user should provide wts=ss.eigenvals    
 
+    '''
 
-	# Compute mult_mu_star_k (if needed)
-	if mult is False:
-		mu_star = mu_star
-	else:
-		mu_star = mu_star*abs(f0)**0.5    
-    
-	
-	# Draw a random vector of same size as x_init
-    
-	if active is None:
-        
-		u = np.random.normal(0,1,(np.size(x_init),1))
-        
-    
-	else:
-        
-		act_dim = active.shape[1]
+    # Evaluate noisy F(x_init)
 
-        
-		if wts is None:
-			lam = np.random.normal(0,1,(act_dim,1))
+    f0 = F(x_init)
 
-		else:
-			lam = np.zeros((act_dim,1))
-			for i in range(act_dim):
-				lam[i]=np.random.normal(0,wts[0][0]/wts[i][0])        
-		
-		u = active@lam
-    
-    
-    
-	
-	# Form vector y, which is a random walk away from x_init
-   
-	y = x_init + (mu_star)*u
-    
-    
-	
-	# Evaluate noisy F(y)
-    
-	g = F(y)
-    
-    
-	
-	# Form finite-difference "gradient oracle"
-    
-	s = ((g - f0)/mu_star)*u 
-    
-    
-	
-	# Take descent step in direction of -s smooth by h to get next iterate, x_1
-    
-	x = x_init - (h)*s
-   
-    
-	
-	# Evaluate noisy F(x_1)
-    
-	f1 = F(x)
-    
-    
-	
-	# Form upper bound for L_1
-    
-	d1 = (f0-g)*(1/mu_star)
-    
-	d2 = (g-f1)*(1/h)
-    
-	avg_step = .5*mu_star**2+.5*h**2
-    
-	L_1_B = abs(d2-d1)/avg_step    
-        
-    
-	
-	return [x, f1, y, g, x_init, f0, L_1_B]
+    # Compute mult_mu_star_k (if needed)
+    if mult is False:
+        mu_star = mu_star
+    else:
+        mu_star = mu_star*abs(f0)**0.5
 
+    # Draw a random vector of same size as x_init
 
-	
-	#if f1<=f0:
-		#return [x, f1, y, g, x_init, f0, L_1_B]
+    if active is None:
 
+        u = np.random.normal(0, 1, (np.size(x_init), 1))
 
-	#else:
-		#return [x_init, f0, y, g, x_init, f1, L_1_B]
+    else:
 
+        act_dim = active.shape[1]
+
+        if wts is None:
+            lam = np.random.normal(0, 1, (act_dim, 1))
+
+        else:
+            lam = np.zeros((act_dim, 1))
+            for i in range(act_dim):
+                lam[i] = np.random.normal(0, wts[0][0]/wts[i][0])
+
+        u = active@lam
+
+    # Form vector y, which is a random walk away from x_init
+
+    y = x_init + (mu_star)*u
+
+    # Evaluate noisy F(y)
+
+    g = F(y)
+
+    # Form finite-difference "gradient oracle"
+
+    s = ((g - f0)/mu_star)*u
+
+    # Take descent step in direction of -s smooth by h to get next iterate, x_1
+
+    x = x_init - (h)*s
+
+    # Evaluate noisy F(x_1)
+
+    f1 = F(x)
+
+    # Form upper bound for L_1
+
+    d1 = (f0-g)*(1/mu_star)
+
+    d2 = (g-f1)*(1/h)
+
+    avg_step = .5*mu_star**2+.5*h**2
+
+    L_1_B = abs(d2-d1)/avg_step
+
+    return [x, f1, y, g, x_init, f0, L_1_B]
+
+    # if f1<=f0:
+    # return [x, f1, y, g, x_init, f0, L_1_B]
+
+    # else:
+    # return [x_init, f0, y, g, x_init, f1, L_1_B]
